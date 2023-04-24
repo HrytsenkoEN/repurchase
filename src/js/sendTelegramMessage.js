41d92c089003c876cd28beafcc663501cb4564bf
HrytsenkoEN <ivgenius@gmail.com>
2023-04-24 23:54:33 +0300

    Update sendTelegramMessage.js

--- src/js/sendTelegramMessage.js
+++ src/js/sendTelegramMessage.js
@@ -5,24 +5,24 @@ const querystring = require('querystring');
 const telegramBotToken = '512482013:AAEMy-W7LTkZNmM95H0yVHKSSVq1Fvt76D8';
 const telegramChatId = '-972517118';
 
-async function sendTelegramMessage(formData) {
-  const message = `Нове повідомлення від ${formData.tel} (${formData.comment}):\n\n${formData.message}`;
-console.log(message);
-  const query = querystring.stringify({
-    chat_id: telegramChatId,
-    text: message
-  });
+// async function sendTelegramMessage(formData) {
+//   const message = `Нове повідомлення від ${formData.tel} (${formData.comment}):\n\n${formData.message}`;
+// console.log(message);
+//   const query = querystring.stringify({
+//     chat_id: telegramChatId,
+//     text: message
+//   });
 
-  const config = {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  };
+//   const config = {
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded'
+//     }
+//   };
 
-  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+//   const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
 
-  await axios.post(url, query, config);
-}
+//   await axios.post(url, query, config);
+// }
 
 const form = document.getElementById('modal-form');
 
