8d22aa97a00f19181aef0369152e2b7a060993c1
HrytsenkoEN <ivgenius@gmail.com>
2023-05-11 11:57:38 +0300

    Update sendTelegramMessage.js

--- src/js/sendTelegramMessage.js
+++ src/js/sendTelegramMessage.js
@@ -5,7 +5,7 @@ const token = '6196909571:AAFOZjAec4WPyliZ5CIK6GSVnGy1XA06-dM';
 const chatId = '-980921121';
 // const token = '512482013:AAEMy-W7LTkZNmM95H0yVHKSSVq1Fvt76D8';
 // const chatId = '-972517118';
-const urlApi = `https://api.telegram.org/bot${ token }/sendMessage`;
+const urlApi = `https://api.telegram.org/bot${token}/sendMessage`;
 // const formData = {};
 
 // updateForm();
@@ -14,49 +14,49 @@ const urlApi = `https://api.telegram.org/bot${ token }/sendMessage`;
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
-      e.preventDefault();
-      
-      let message = `Нашли жертву\n`;
-      message += `телефон: ${ this.tel.value }\n`;
-      message += `машина: ${ this.brand.value }\n`;
-      message += `модель:  ${ this.model.value }\n`;
-      message += `ціна:  ${ this.price.value }\n`;
-      message += `коментар:  ${ this.comment.value }\n`;
+  e.preventDefault();
 
+  let message = `Нашли жертву\n`;
+  message += `телефон: ${this.tel.value}\n`;
+  message += `машина: ${this.brand.value}\n`;
+  message += `модель:  ${this.model.value}\n`;
+  message += `ціна:  ${this.price.value}\n`;
+  message += `коментар:  ${this.comment.value}\n`;
 
-
-      axios.post(urlApi, {
-        chat_id: chatId,
-        parce_mode: 'html',
-        text: message
-      })
-          .then((res) => {
-            this.tel.value = '';
-            this.brand.value = '';
-            this.model.value = '';
-            this.price.value = '';
-            this.comment.value = '';
-          })
-          .catch((err) => {
-            if (tel.value === '' || 
-            brand.value === ''  || 
-            model.value === ''  || 
-            price.value === '') {
-              return Notify.failure(
-                'Заповни всі поля!'
-              );}
-          })
-          .finally(() => {
-            console.log({ 
-              tel: tel.value, 
-              brand: brand.value, 
-              model: model.value, 
-              price: price.value, 
-              comment: comment.value });
-              evt.target.reset();
-          })
+  axios
+    .post(urlApi, {
+      chat_id: chatId,
+      parce_mode: 'html',
+      text: message,
+    })
+    .then(res => {
+      this.tel.value = '';
+      this.brand.value = '';
+      this.model.value = '';
+      this.price.value = '';
+      this.comment.value = '';
+    })
+    .catch(err => {
+      if (
+        tel.value === '' ||
+        brand.value === '' ||
+        model.value === '' ||
+        price.value === ''
+      ) {
+        return Notify.failure('Заповни всі поля!');
+      }
+    })
+    .finally(() => {
+      Notify.success('Hooray! We send formdata.');
+      console.log({
+        tel: tel.value,
+        brand: brand.value,
+        model: model.value,
+        price: price.value,
+        comment: comment.value,
+      });
+    });
 }
-
 
 // async function sendTelegramMessage(formData) {
 //   const message = `Нове повідомлення від ${formData.tel} (${formData.comment}):\n\n${formData.message}`;
